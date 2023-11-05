cfe816fbc9d3e85cf3c2ca3e8d2873c0b93985f8
Kevin Agyeman <kevinagyemann@gmail.com>
2023-11-05 04:02:42 +0100

    score

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import Index from './pages';
+import UpdateScore from './components/restaurant-update-score';
 
 export default function App() {
   return (
     <>
       <Routes>
         <Route index element={<Index />} />
+        <Route path='/update-score' element={<UpdateScore />} />
       </Routes>
     </>
   );
